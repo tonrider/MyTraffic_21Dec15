54cdf0a2e373eecfb4a828ab2baf5c025941a599
tonrider <ton0422@gmail.com>
2015-12-21 16:41:24 +0700

    Finish Day1

--- app/src/main/java/com/example/korsak_c/mytraffic/MainActivity.java
+++ app/src/main/java/com/example/korsak_c/mytraffic/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.korsak_c.mytraffic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Explicit  ประกาศตัวแปร
+    private ListView trafficListView;
+    private Button aboutMeButton;
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bind Widget
+        bindWidget();
+
+    }   // Main Method
+
+    private void bindWidget() {
+
+        trafficListView = (ListView) findViewById(R.id.listView);
+        aboutMeButton = (Button) findViewById(R.id.button);
+
+
     }
-}
+
+}   // Main Class
+
