1cae4897b87542479d0506cb046d7f4ff27d7242
tonrider <ton0422@gmail.com>
2015-12-21 14:21:23 +0700

    test1

---
+++ app/src/main/java/com/example/korsak_c/mytraffic/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.korsak_c.mytraffic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
