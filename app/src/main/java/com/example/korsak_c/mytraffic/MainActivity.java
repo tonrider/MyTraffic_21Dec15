25ea7fd7f8215b7e4ee19f261c362e70f4ccb7d9
tonrider <ton0422@gmail.com>
2015-12-22 10:26:34 +0700

    ButtonController เสร็จแล้ว

--- app/src/main/java/com/example/korsak_c/mytraffic/MainActivity.java
+++ app/src/main/java/com/example/korsak_c/mytraffic/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.korsak_c.mytraffic;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
@@ -24,7 +28,27 @@ public class MainActivity extends AppCompatActivity {
         //Bind Widget
         bindWidget();
 
+        //Button Controller
+        buttonController();
+
     }   // Main Method
+
+    private void buttonController() {
+        aboutMeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MediaPlayer buttonMediaPlayer = MediaPlayer.create(getBaseContext(), R.raw.crickets);
+                buttonMediaPlayer.start();
+
+                Intent objIntent = new Intent(Intent.ACTION_VIEW);
+                objIntent.setData(Uri.parse("https://youtu.be/yKh5WvzwYOo"));
+                startActivity(objIntent);
+
+            }   //event
+        });
+
+    }
 
     private void bindWidget() {
 
